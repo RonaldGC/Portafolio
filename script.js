b0d94376444c1ce69eebd63ae4bb51aed2f4524f
ronaldgc <ronald.gc3154@gmail.com>
2024-11-14 19:25:17 -0600

    - Se añadieron alts - Se animo la navbar - Se posiciono y animo el menu de contacto - Se añadio el boton para subir a home 	modified:   portafolio.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function toggleContactMenu() {
     const contactMenu = document.getElementById('contact-menu');
-    contactMenu.classList.toggle('show');
+    
+    if (contactMenu.classList.contains('show')) {
+        contactMenu.classList.remove('show');
+        contactMenu.classList.add('none');
+
+        
+        setTimeout(() => {
+            contactMenu.style.display = 'none';
+        }, 300); 
+    } else {
+        contactMenu.style.display = 'flex';
+        contactMenu.classList.remove('hidden');
+        contactMenu.classList.add('show');
+    }
 }
+
 
