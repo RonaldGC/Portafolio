421dbf8ecf8ae4993598aa3d646def857018f410
ronaldgc <ronald.gc3154@gmail.com>
2024-11-18 22:19:57 -0600

    - Se cambia l a foto del formulario
    	new file:   Extras/Foto_mia.png
    	deleted:    Extras/Foto_mia.webp
    	modified:   script.js
    	modified:   style.css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const contactIcon = document.getElementById('contact-icon');
-    
+
     if (contactIcon) {
         contactIcon.addEventListener('click', function () {
             toggleVisibility('contact-menu');
@@ -9,14 +9,15 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error("El elemento con ID 'contact-icon' no se encontró en el DOM.");
     }
 
-    const formButton = document.getElementById('form-button');
+    const formButton = document.getElementById('boton-formulario');
     if (formButton) {
         formButton.addEventListener('click', function () {
             toggleVisibility('formulario-contacto');
         });
     } else {
-        console.error("El elemento con ID 'form-button' no se encontró en el DOM.");
+        console.error("No se encontró el elemento ID boton-formulario");
     }
+    
 });
 
 function toggleVisibility(elementId) {
@@ -30,20 +31,14 @@ function toggleVisibility(elementId) {
             setTimeout(() => {
                 element.classList.add('hidden'); 
                 console.log("Nuevo estado del elemento después de ocultar:", element.classList);
-            }); 
+            }), 300; 
         } else if (element.classList.contains('hidden')) {
             element.classList.remove('hidden'); 
             setTimeout(() => {
                 element.classList.add('show');
                 console.log("Nuevo estado del elemento después de mostrar:", element.classList);
-            }); 
+            }), 0; 
         }
     }
 }
-
-
-
-
-
-
 
